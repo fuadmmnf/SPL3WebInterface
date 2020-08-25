889e86096e02fbf8f1b8cc9f7654f0023f0f67da
fuadmmnf <bsse0920@iit.du.ac.bd>
2020-08-25 15:02:37 +0600

    added loading and content card

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,36 +1,52 @@
 <template>
-  <div class="row justify-center" style="margin-top: 20%">
-    <q-card v-if="!results.length" class="my-card">
-      <q-card-section class="row">
-        <div class="row">
-          <q-file
-              class="col"
-              v-model="files"
-              label="Pick Java Source files"
-              filled
-              multiple
-              style="width: 400px"
-          >
-            <template v-slot:before>
-              <q-icon name="code"/>
-            </template>
+  <div>
+    <div class="row justify-center q-ma-md">
+      <q-card class="my-card">
+        <q-card-section>
+          <div class="row">
+            <q-file
+                class="col"
+                v-model="files"
+                label="Pick Java Source files"
+                filled
+                multiple
+                style="width: 400px"
+            >
+              <template v-slot:before>
+                <q-icon name="code"/>
+              </template>
 
-            <template v-slot:append>
-              <q-btn round dense flat icon="clear" @click="files = null"/>
-            </template>
-          </q-file>
-          <q-btn class="col-2 q-ma-md" unelevated color="primary" label="proceed" @click="detectFileSimilarity"/>
-        </div>
-      </q-card-section>
-    </q-card>
+              <template v-slot:append>
+                <q-btn round dense flat icon="clear" @click="files = null"/>
+              </template>
+            </q-file>
+            <q-btn class="col-2 q-ma-md" unelevated color="primary" label="proceed" @click="detectFileSimilarity"/>
+          </div>
+        </q-card-section>
+      </q-card>
 
-    <q-card v-else class="my-card">
-      <q-card-section>
-        <div class="">
 
-        </div>
-      </q-card-section>
-    </q-card>
+    </div>
+
+    <div v-if="isLoading || results.length" class="row justify-center q-ma-md">
+      <q-card class="my-card" style="width: 45%; height: 50%">
+        <q-card-section class="row justify-center">
+          <div v-if="isLoading">
+            <q-circular-progress
+                indeterminate
+                size="50px"
+                :thickness="0.22"
+                color="blue"
+                track-color="grey-3"
+                class="q-ma-md"
+            />
+          </div>
+          <div v-else>
+
+          </div>
+        </q-card-section>
+      </q-card>
+    </div>
   </div>
 </template>
 <script>
@@ -39,6 +55,7 @@ import axios from 'axios';
 export default {
   data() {
     return {
+      isLoading: true,
       files: null,
       sourceCodes: [],
       results: []
@@ -67,6 +84,5 @@ export default {
   }
 }
 </script>
-<style lang="sass">
-
+<style scoped>
 </style>
